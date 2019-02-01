49bacad6ca665790a5bc0e35f2e90450ffc56a8f
Katrina Hockman <khockman90@hotmail.com>
2019-02-01 14:24:16 -0800

    Attempt to retrieve data from firebase, error says client doesnot have permission

--- src/app/open-access/open-access.component.ts
+++ src/app/open-access/open-access.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { ArtService } from '../art.service';
+import { Router } from '@angular/router';
+import { Art } from '../models/art.model';
+
 
 @Component({
   selector: 'app-open-access',
   templateUrl: './open-access.component.html',
-  styleUrls: ['./open-access.component.css']
+  styleUrls: ['./open-access.component.css'],
+  providers: [ArtService]
+
 })
 export class OpenAccessComponent implements OnInit {
     artworks: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private router: Router, private artService: ArtService) { }
 
   ngOnInit() {
-  }
+      this.artworks = this.artService.getArt();
+    }
 
 }
